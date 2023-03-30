a62de1fe4e0f5fb6ab81026ab9e4b2d884afcee7
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2023-03-30 21:36:41 +0200

    Finished working text display

--- TextDisplay/script.js
+++ TextDisplay/script.js
@@ -1,3 +1,8 @@
 function displayText() {
     var text = document.getElementById("displayText");
+    if (text.style.display === "none") {
+        text.style.display = "block";
+      } else {
+        text.style.display = "none";
+      }
 }
